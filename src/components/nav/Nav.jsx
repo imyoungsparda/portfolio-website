3c5ef3da9f4db797cd8ac9a6c1f8ed8dcdd8dbab
Ayush <rajayush1310@gmail.com>
2023-08-10 12:37:41 +0530

    update hover nav icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from "react";
 
 import { AiOutlineHome, AiOutlineUser, AiOutlineBook } from "react-icons/ai";
@@ -13,42 +14,72 @@ function Nav() {
     setActiveNav(id);
   }
   return (
-    <nav>
-      <a
-        href="#"
-        className={activeNav === "#" ? "active" : ""}
-        onClick={() => handleClickNav("#")}
-      >
-        <AiOutlineHome />
-      </a>
-      <a
-        href="#about"
-        className={activeNav === "#about" ? "active" : ""}
-        onClick={() => handleClickNav("#about")}
-      >
-        <AiOutlineUser />
-      </a>
-      <a
-        href="#skill"
-        className={activeNav === "#experience" ? "active" : ""}
-        onClick={() => handleClickNav("#experience")}
-      >
-        <AiOutlineBook />
-      </a>
-      <a
-        href="#portfolio"
-        className={activeNav === "#services" ? "active" : ""}
-        onClick={() => handleClickNav("#services")}
-      >
-        <BsFillBagHeartFill />
-      </a>
-      <a
-        href="#contact"
-        className={activeNav === "#contact" ? "active" : ""}
-        onClick={() => handleClickNav("#contact")}
-      >
-        <BiMessageDetail />
-      </a>
+    <nav className="wrapper">
+      <div className="icon home">
+        <div className="tooltip">Home</div>
+        <span className={activeNav === "#" ? "active" : ""}>
+          <a
+            href="#"
+            className={activeNav === "#" ? "active" : ""}
+            onClick={() => handleClickNav("#")}
+          >
+            <AiOutlineHome />
+          </a>
+        </span>
+      </div>
+
+      <div className="icon about">
+        <div className="tooltip">About</div>
+        <span className={activeNav === "#about" ? "active" : ""}>
+          <a
+            href="#about"
+            className={activeNav === "#about" ? "active" : ""}
+            onClick={() => handleClickNav("#about")}
+          >
+            <AiOutlineUser />
+          </a>
+        </span>
+      </div>
+
+      <div className="icon skill">
+        <div className="tooltip">Skills</div>
+        <span className={activeNav === "#skill" ? "active" : ""}>
+          <a
+            href="#skill"
+            className={activeNav === "#skill" ? "active" : ""}
+            onClick={() => handleClickNav("#skill")}
+          >
+            <AiOutlineBook />
+          </a>
+        </span>
+      </div>
+
+      <div className="icon portfolio">
+        <div className="tooltip">Portfolio</div>
+
+        <span className={activeNav === "#portfolio" ? "active" : ""}>
+          <a
+            href="#portfolio"
+            className={activeNav === "#portfolio" ? "active" : ""}
+            onClick={() => handleClickNav("#portfolio")}
+          >
+            <BsFillBagHeartFill />
+          </a>
+        </span>
+      </div>
+
+      <div className="icon contact">
+        <div className="tooltip">Contact</div>
+        <span className={activeNav === "#contact" ? "active" : ""}>
+          <a
+            href="#contact"
+            className={activeNav === "#contact" ? "active" : ""}
+            onClick={() => handleClickNav("#contact")}
+          >
+            <BiMessageDetail />
+          </a>
+        </span>
+      </div>
     </nav>
   );
 }
